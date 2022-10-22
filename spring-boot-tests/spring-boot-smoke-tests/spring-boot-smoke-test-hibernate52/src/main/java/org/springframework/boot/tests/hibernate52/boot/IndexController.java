fde169b14844ea3705b71d94558109411c2cd1ff
lilei58 <13186102535@163.com>
2022-10-22 10:16:08 +0800

    master springboot 启动

---
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-hibernate52/src/main/java/org/springframework/boot/tests/hibernate52/boot/IndexController.java
@@ -0,0 +1,39 @@
+package org.springframework.boot.tests.hibernate52.boot;
+
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+
+//import org.springframework.stereotype.Controller;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+
+/**
+ * @Description:
+ * @Author: lilei58
+ * @Date: Created in 2021/10/12 上午7:38
+ */
+//@Controller
+//public class IndexController {
+//
+//	@RequestMapping("/index")
+//	@ResponseBody
+//	public Map<String, String> index() {
+//		System.out.println("index");
+//
+//		Map<String, String> map = new HashMap<>(1);
+//		map.put("index", "index");
+//
+//		return map;
+//	}
+//
+//	@RequestMapping("/name")
+//	public String name() {
+//		return "name";
+//	}
+//
+//}
